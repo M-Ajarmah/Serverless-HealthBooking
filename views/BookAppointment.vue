fefb8280273337eba28e086bd2d564bd80c49e8b
M-Ajarmah <almanasah.momen@gmail.com>
2025-06-21 11:32:40 +0300

    Update BookAppointment.vue

--- views/BookAppointment.vue
+++ views/BookAppointment.vue
@@ -47,7 +47,7 @@ export default {
     };
   },
   mounted() {
-    fetch("https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/slots")
+    fetch("https://ytqkg1qjl6.execute-api.us-east-1.amazonaws.com/stage01/slots")
       .then(res => res.json())
       .then(data => {
         const parsed = JSON.parse(data.body);
@@ -62,7 +62,7 @@ export default {
         slot: this.selectedSlot
       };
 
-      fetch("https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/appointments", {
+      fetch("https://ytqkg1qjl6.execute-api.us-east-1.amazonaws.com/stage01/Appointment", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ body: JSON.stringify(payload) })
